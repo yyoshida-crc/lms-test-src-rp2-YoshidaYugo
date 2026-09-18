008e092bf5362d5fcc52f9f58c7d8ec55ecc0291
yyoshida <yyoshida@CRCO01202606005.crc-okinawa.com>
2026-09-18 16:42:15 +0900

    fix:テストケース03 エビデンスの追加
    ケース03クラスtest01メソッドのエビデンス取得

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -43,6 +43,9 @@ public class Case03 {
 		WebDriverUtils.goTo("http://localhost:8080/lms");
 
 		assertEquals("ログイン | LMS", WebDriverUtils.webDriver.getTitle());
+		WebDriverUtils.getEvidence(new Object() {
+
+		});
 	}
 
 	@Test
