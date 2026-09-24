b2a7a8dd74bc6ce3f585392c2d02ba0c33653768
yyoshida <yyoshida@CRCO01202606005.crc-okinawa.com>
2026-09-24 15:43:11 +0900

    add:テストケース03 コメント、テスト仕様書の追加
    ソースコード
    //TODO コメントの削除
    
    テスト仕様書
    RP2_10_結合テスト仕様書_1.1の追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -39,7 +39,7 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
 		WebDriverUtils.goTo("http://localhost:8080/lms");
 
 		assertEquals("ログイン | LMS", WebDriverUtils.webDriver.getTitle());
@@ -52,7 +52,7 @@ public class Case03 {
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
 		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
 		webDriver.findElement(By.id("password")).sendKeys("PasswordAA01");
 		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
